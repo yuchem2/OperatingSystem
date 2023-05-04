3cda00ab99fcec09a7b70f780ec27e129d74012d
Yoon Jaehyun <yuchem2@gmail.com>
2023-05-04 14:13:57 +0900

    Update main.cpp

--- ProcessCounter/main.cpp
+++ ProcessCounter/main.cpp
@@ -4,15 +4,20 @@
 #include <sysinfoapi.h>
 using namespace std;
 
+// Change SYSTEMTIME instance to a character array according to the specified format. 
 void makeStr(SYSTEMTIME *time, char *str) {
 	sprintf_s(str, 33, "%04d.%02d.%02d %02d:%02d:%02d : ", time->wYear, time->wMonth, time->wDay, time->wHour, time->wMinute, time->wSecond);
 	return;
 }
 
+/* 
+	Real work function
+	Check the number of processes currently running and print it out every seconds with the current time
+*/
 void printOut() {
 	DWORD aProcesses[1024], cbNeeded, cProcesses;
 	SYSTEMTIME currentTime, afterTime;
-	bool First = TRUE;
+	bool first = TRUE;
 
 	while (EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
 		char str[33];
@@ -21,7 +26,7 @@ void printOut() {
 			GetLocalTime(&currentTime);
 			makeStr(&currentTime, str);
 			cout << str << cProcesses << endl;
-			First = FALSE;
+			first = FALSE;
 		}
 		else {
 			GetLocalTime(&afterTime);
